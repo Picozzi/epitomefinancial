51769393a6ff703fc0a534d4f605204684cefe58
Matthew Picozzi <matthew.picozzi@gmail.com>
2020-12-09 13:31:19 -0500

    heroku install

--- epitome_financial/settings.py
+++ epitome_financial/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -153,3 +154,5 @@ AWS_DEFAULT_ACL = None
 AWS_S3_ADDRESSING_STYLE = "virtual"
 AWS_S3_REGION_NAME= 'us-east-2'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals()) 
