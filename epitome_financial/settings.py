7c4b2e724de8a00870a9f72e9af598558e900b62
Matthew Picozzi <matthew.picozzi@gmail.com>
2020-12-09 13:32:18 -0500

    changing debug value

--- epitome_financial/settings.py
+++ epitome_financial/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG_VALUE')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['epitomefinancial.herokuapp.com']
 
